d73e27e4b5b6fef116fbc1dc8931eb06ad1ec09f
Ian Linsdell <Ian.Linsdell@verint.com>
2015-12-17 13:23:15 -0500

    N/A Encapculate WEBWIZ in a namespace

--- Dependencies/WebWizard_1_0/webwizard.js
+++ Dependencies/WebWizard_1_0/webwizard.js
@@ -28,8 +28,10 @@
  * 3. Refresh your WFO browser to force it to reload your updated code.
  */
 
-// TODO Load work flow JSON from file, managing cross domain security issues.
-var wizardflows = $.parseJSON('{' +
+var WEBWIZ = {
+
+	// TODO Load work flow JSON from file, managing cross domain security issues.
+	wizflows: $.parseJSON('{' +
          '"workflows": [{' +
              '"type": "menuitem",' +				// Configuration work flow menu item
              '"title": "Configure Data Source",' +
@@ -113,281 +115,286 @@ var wizardflows = $.parseJSON('{' +
 	         '"y": 700,' +
              '"proceed": {"type":"mousedown","target":{"outerText": "Save"}}'+
          '}]' + 
-	  '}');
+	  '}'),
 
-// Show Wizard configuration Notification
-var wizardtoast = function(wizardstep) {
+	// Selected work flow and current step in the wizard
+	flowselected: -1,
+    currentstep: -1,
+
+	// MainWindow and wizard steps
+    mainWin: null,
+	wizsteps: [],
+
+	// Initialize web wizard with this web application
+	initialize: function(mainWindow) {
 		
-	$("<div class='ui-loader ui-overlay-shadow ui-body-e ui-corner-all' id='divtoast'><h3 id='toast'>" + wizardstep.title + "</h3></div>")
-    .css({ display: "block",
-         background: "#0C99C9",
-         border: "2px solid black",
-         "border-radius": "10px",
-         "box-shadow": "10px 10px 5px #888888",
-         opacity: 0.90,
-         position: "fixed",
-         padding: "7px",
-         "text-align": "center",
-		 color:"white",
-		 "z-index": "130010",		// Higher that the WFO navigation bar of 130000
-         width: "270px",
-         left: wizardstep.x,
-         top: wizardstep.y})
-    .appendTo("body")
-    .fadeOut(0)
-    .fadeIn(500, function() {
-       // Animation complete.
-    }); 
-}
-
-// Show Wizard configuration flow complete Notification
-var wizardflowcomplete = function() {
-	
-	$("<div class='ui-loader ui-overlay-shadow ui-body-e ui-corner-all' id='divtoast'><h3 id='toast'>Configuration Complete</h3></div>")
-    .css({ display: "block",
-         background: "#0C99C9",
-         border: "2px solid black",
-         "border-radius": "10px",
-         "box-shadow": "10px 10px 5px #888888",
-         opacity: 0.90,
-         position: "fixed",
-         padding: "7px",
-         "text-align": "center",
-         "z-index": "19020",		// Higher that the WFO navigation bar of 19000 & 19020
-		 color:"white",
-         width: "270px",
-         left: ($(window).width() - 284)/2,
-         top: $(window).height()/2})
-    .appendTo("body")
-    .fadeOut(0)
-    .fadeIn(500, function() {
-       // fadeIn Complete
-    })    
-    .delay(3000)
-    .fadeOut(400, function(){
-        $(this).remove();
-    });
-}
-
-// Show Wizard menu
-var wizardmenu = function(x, y, menu) {
-	
-	$("<div class='ui-loader ui-overlay-shadow ui-body-e ui-corner-all' id='divwizmenu'><h3>" + menu + "</h3></div>")
-	  .css({ display: "block",
-         background: "#0C99C9",
-         border: "2px solid black",
-         "border-radius": "10px",
-         "box-shadow": "10px 10px 5px #888888",
-         opacity: 0.90,
-         position: "fixed",
-         padding: "7px",
-         "text-align": "center",
-		 color:"white",
-         width: "270px",
-         left: x,
-         top: y })
-     .appendTo("body")
-     .fadeOut(0)
-     .fadeIn(300, function() {
-        // fadeIn Complete
-     })    
-}
-
-/*
-  -webkit-box-shadow: 0px 9px 0px rgba(219,31,5,1), 0px 9px 25px rgba(0,0,0,.7);
-    -moz-box-shadow: 0px 9px 0px rgba(219,31,5,1), 0px 9px 25px rgba(0,0,0,.7);
-    box-shadow: 0px 9px 0px rgba(219,31,5,1), 0px 9px 25px rgba(0,0,0,.7);
- */
-
-// Populate Work flows Steps
-var wizardsteps = [];
-for (menuIdx in wizardflows.workflows) {
-    if (wizardflows.workflows[menuIdx].type === 'step') {
-    	wizardsteps.push(wizardflows.workflows[menuIdx]);
-    }
-}
-
-// Return the current step from the current wizard flow
-var getwizardflowstep = function() {
-	return wizardflows.workflows[wizardflowselected].steps[wizardcurrentstep];
-}
-
-// Return the current work flow step
-var getwizardstep = function(stepIdx) {
-	for (idx in wizardsteps) {
-		if (wizardsteps[idx].id === stepIdx) {
-			return wizardsteps[idx];
+		// Save application main document window
+		if (WEBWIZ.mainWin === null) {
+			WEBWIZ.mainWin = mainWindow;
 		}
-	}
-	return null;
-}
-
-// Log only if console is available
-var wizlog = function(obj) {
-	if (window.console) {
-		console.log(obj);
-	}
-}
-
-// Selected work flow and current step in the wizard
-var wizardflowselected = -1;
-var wizardcurrentstep = -1;
-
-// Handle next step in the wizard
-var wizardnextstep = function() {
-	
-	// Remove menu if displayed
-	$('#divwizmenu').remove();
-	
-	// Remove any existing toast
-	$('#divtoast').remove();
-
-	// Move onto the next toast in the work flow
-	wizardcurrentstep++;	
-	var nextstep = getwizardflowstep();
-	if (nextstep === undefined) {
-		wizardflowcomplete();
-		wizardcurrentstep = -1;
-	} else {
-		wizardtoast(getwizardstep(nextstep.step));
-	}
-}
-
-// Wizard mouse/key down intercept handler
-var wizardeventhandler = function(event) {
-	
-	// Display configuration work flow wizard if CTRL key is held during mouse down
-	if (event.ctrlKey === true && event.type === 'mousedown') {
 		
-		// Prevent default action of the event from being triggered.
-		event.preventDefault();
-		  		
-		// Custom event handler
-		var x = ($(window).width() - 284)/2;
-		var y = $(window).height()/2;
-
-		var menu = '';
-		for (menuIdx in wizardflows.workflows) {
-		    if (wizardflows.workflows[menuIdx].type === 'menuitem') {
-		    	menu += '<a style="color:white;text-decoration:none" href="#" onmousedown="wizardflowselected=' + menuIdx + ';wizardcurrentstep=-1;wizardnextstep();">' + wizardflows.workflows[menuIdx].title  + '</a></br>';
-		    }
+		// Populate Work flows Steps
+		for (var menuIdx in WEBWIZ.wizflows.workflows) {
+    		if (WEBWIZ.wizflows.workflows[menuIdx].type === 'step') {
+    			WEBWIZ.wizsteps.push(WEBWIZ.wizflows.workflows[menuIdx]);
+    		}
 		}
-		wizardmenu(x, y , menu);	
-	} else {
 		
-		if (event.ctrlKey === true) return;
+		// Add event listners to every <body> of every <iframe>
+		WEBWIZ.wizhookevents();	
+	},
 		
-		wizlog(event);
-
+	// Intercept all mouse/key down on all body elements
+	wizhookevents: function() {
+		
+		var handlerattached = false;
+	
+		// Use $._data(element,'events') to retrieve existing events attached to the element.	
+		var bodyevents = $._data($('body')[0],'events');
+		if (bodyevents !== undefined) {
+			// Iterate each attached mousedown event handler
+			$.each(bodyevents.mousedown, function(evtguid, funcobj) {
+				// Check if wizeventhandler is already attached to element
+				if (funcobj.handler === WEBWIZ.wizventhandler) {
+					handlerattached = true;
+				}
+			});
+		}
+			
+		// If the wizard mousedown handler is not already attached, attach it
+		if (handlerattached === false) {
+			$('body').on('mousedown', WEBWIZ.wizeventhandler);
+			$('body').on('keydown', WEBWIZ.wizeventhandler);
+			WEBWIZ.wizlog('Wizard attach event hooks to bodys:' + $('body').length + ' iframes:' + $('iframe').length);
+		}
+		
+		WEBWIZ.wizeventhookiframes(WEBWIZ.mainWin.document);
+				
+		setTimeout(function() {
+			WEBWIZ.wizhookevents();
+		}, 1000);
+	},
+	
+	// Handle next step in the wizard
+	wiznextstep: function() {
+		
 		// Remove menu if displayed
 		$('#divwizmenu').remove();
-
-		// Intercept application mousedown to proceed onto the next wizard step
-		if (wizardcurrentstep > -1) {
-			
-			var step = getwizardstep(getwizardflowstep().step);
-			if (event.type === step.proceed.type) {
-				
-				// Iterate target variables that need to be matched and check against event target properties
-				// All event target properties must be matched in AND comparison
-				var match = true;
-				for (var name in step.proceed.target) {
-					if (event.target[name] !== undefined && event.target[name].indexOf(step.proceed.target[name]) > -1) {
-						match = true;
-					} else {
-						match = false;
-					}
-				}
-				if (match === true) {
-					wizardnextstep();
-				}
-			}			
-		}
-	}
-};
-
-// Add mouse/key down event listener to all nested iframe document elements
-wizardeventhookiframes = function(myEle) {
-
-	// Iterate each iframe nested in myEle
-	$.each($(myEle).find("iframe"), function() {
-
-		try {
-			var iframedoc = $(this)[0].contentWindow.document; // may need to change this depending on browser
-			var iframebody = $(iframedoc).find("body");
+		
+		// Remove any existing toast
+		$('#divtoast').remove();
 	
-			var handlerattached = false;
+		// Move onto the next toast in the work flow
+		WEBWIZ.wizcurrentstep++;	
+		var nextstep = WEBWIZ.getwizflowstep();
+		if (nextstep === undefined) {
+			WEBWIZ.wizflowcomplete();
+			WEBWIZ.wizcurrentstep = -1;
+		} else {
+			WEBWIZ.wiztoast(WEBWIZ.getwizstep(nextstep.step));
+		}
+	},
+	
+	// Wizard mouse/key down intercept handler
+	wizeventhandler: function(event) {
+		
+		// Display configuration work flow wizard if CTRL key is held during mouse down
+		if (event.ctrlKey === true && event.type === 'mousedown') {
 			
-			// Use $._data(element,'events') to retrieve existing events attached to the element.	
-			var iframebodyevents = $._data(iframebody[0],'events');
-			if (iframebodyevents !== undefined && iframebodyevents.mousedown !== undefined) {
-				$.each(iframebodyevents.mousedown, function(evtguid, funcobj) {
-				    // Check if wizardeventhandler is already attached to element
-					if (funcobj.handler === wizardeventhandler) {
-				    	handlerattached = true;
-				    }
-				});
-			} 
-			
-			// If event handler is not attached to iframe, attach it
-			if (handlerattached === false) {
-				$(iframebody).on('mousedown', wizardeventhandler);
-				$(iframebody).on('keydown', wizardeventhandler);
-				wizlog('Attached event handler to iframe id:' + this.id + ' body element');
+			// Prevent default action of the event from being triggered.
+			event.preventDefault();
+					
+			// Custom event handler
+			var x = ($(window).width() - 284)/2;
+			var y = $(window).height()/2;
+	
+			var menu = '';
+			for (var menuIdx in WEBWIZ.wizflows.workflows) {
+				if (WEBWIZ.wizflows.workflows[menuIdx].type === 'menuitem') {
+					menu += '<a style="color:white;text-decoration:none" href="#" onmousedown="WEBWIZ.wizflowselected=' + menuIdx + ';WEBWIZ.wizcurrentstep=-1;WEBWIZ.wiznextstep();">' + WEBWIZ.wizflows.workflows[menuIdx].title  + '</a></br>';
+				}
 			}
-		
-			// Recursivly call to hook into nested iframes 
-			wizardeventhookiframes(iframedoc);
-		} catch(err) {
-			// Ignore undefined exception race condition if iframe created but body does not yet exist
-			// Handler attach will be retried next timeout
-			wizlog('wizardeventhookiframes.' + err);
-		}
-		
-	});
-}
-
-// Intercept all mouse/key down on all body elements
-var wizardeventhook = function() {
-	
-	var handlerattached = false;
-
-	// Use $._data(element,'events') to retrieve existing events attached to the element.	
-	var bodyevents = $._data($('body')[0],'events');
-	if (bodyevents !== undefined) {
-		// Iterate each attached mousedown event handler
-		$.each(bodyevents.mousedown, function(evtguid, funcobj) {
-		    // Check if wizardeventhandler is already attached to element
-			if (funcobj.handler === wizardeventhandler) {
-		    	handlerattached = true;
-		    }
-		});
-	}
-		
-	// If the wizard mousedown handler is not already attached, attach it
-	if (handlerattached === false) {
-		$('body').on('mousedown', wizardeventhandler);
-		$('body').on('keydown', wizardeventhandler);
-		wizlog('Wizard attach event hooks to bodys:' + $('body').length + ' iframes:' + $('iframe').length);
-	}
-	
-	wizardeventhookiframes(wizardWin.document);
+			WEBWIZ.wizmenu(x, y , menu);
+				
+		} else {
 			
-	setTimeout(function() {
-		wizardeventhook();
-	}, 1000);
+			if (event.ctrlKey === true) return;
+			
+			WEBWIZ.wizlog(event);
+	
+			// Remove menu if displayed
+			$('#divwizmenu').remove();
+			
+			// Intercept application mousedown to proceed onto the next wizard step
+			if (WEBWIZ.wizcurrentstep > -1) {
+				
+				var step = WEBWIZ.getwizstep(WEBWIZ.getwizflowstep().step);
+				if (event.type === step.proceed.type) {
+					
+					// Iterate target variables that need to be matched and check against event target properties
+					// All event target properties must be matched in AND comparison
+					var match = true;
+					for (var name in step.proceed.target) {
+						if (event.target[name] !== undefined && event.target[name].indexOf(step.proceed.target[name]) > -1) {
+							match = true;
+						} else {
+							match = false;
+						}
+					}
+					if (match === true) {
+						WEBWIZ.wiznextstep();
+					}
+				}			
+			}
+		}
+	},
+	
+	// Add mouse/key down event listener to all nested iframe document elements
+	wizeventhookiframes: function(myEle) {
+	
+		// Iterate each iframe nested in myEle
+		$.each($(myEle).find("iframe"), function() {
+	
+			try {
+				var iframedoc = $(this)[0].contentWindow.document; // may need to change this depending on browser
+				var iframebody = $(iframedoc).find("body");
+		
+				var handlerattached = false;
+				
+				// Use $._data(element,'events') to retrieve existing events attached to the element.	
+				var iframebodyevents = $._data(iframebody[0],'events');
+				if (iframebodyevents !== undefined && iframebodyevents.mousedown !== undefined) {
+					$.each(iframebodyevents.mousedown, function(evtguid, funcobj) {
+						// Check if wizardeventhandler is already attached to element
+						if (funcobj.handler === WEBWIZ.wizeventhandler) {
+							handlerattached = true;
+						}
+					});
+				} 
+				
+				// If event handler is not attached to iframe, attach it
+				if (handlerattached === false) {
+					$(iframebody).on('mousedown', WEBWIZ.wizeventhandler);
+					$(iframebody).on('keydown', WEBWIZ.wizeventhandler);
+					WEBWIZ.wizlog('Attached event handler to iframe id:' + this.id + ' body element');
+				}
+			
+				// Recursivly call to hook into nested iframes 
+				WEBWIZ.wizeventhookiframes(iframedoc);
+			} catch(err) {
+				// Ignore undefined exception race condition if iframe created but body does not yet exist
+				// Handler attach will be retried next timeout
+				WEBWIZ.wizlog('wizeventhookiframes.' + err);
+			}
+			
+		});
+	},	
+	
+	// Return the current step from the current wizard flow
+	getwizflowstep: function() {
+		return WEBWIZ.wizflows.workflows[WEBWIZ.wizflowselected].steps[WEBWIZ.wizcurrentstep];
+	},
+
+	// Return the current work flow step
+	getwizstep: function(stepIdx) {
+		for (var idx in WEBWIZ.wizsteps) {
+			if (WEBWIZ.wizsteps[idx].id === stepIdx) {
+				return WEBWIZ.wizsteps[idx];
+			}
+		}
+		return null;
+	},
+
+	// Log only if console is available
+	wizlog: function(obj) {
+		if (window.console) {
+			console.log(obj);
+		}
+	},
+
+	// Show Wizard configuration Notification
+	wiztoast: function(wizardstep) {
+		$("<div class='ui-loader ui-overlay-shadow ui-body-e ui-corner-all' id='divtoast'><h3 id='toast'>" + wizardstep.title + "</h3></div>")
+		.css({ display: "block",
+			background: "#0C99C9",
+			border: "2px solid black",
+			"border-radius": "10px",
+			"box-shadow": "10px 10px 5px #888888",
+			opacity: 0.90,
+			position: "fixed",
+			padding: "7px",
+			"text-align": "center",
+			color:"white",
+			"z-index": "130010",		// Higher that the WFO navigation bar of 130000
+			width: "270px",
+			left: wizardstep.x,
+			top: wizardstep.y})
+		.appendTo("body")
+		.fadeOut(0)
+		.fadeIn(500, function() {
+		// Animation complete.
+		});
+	},
+
+	// Show Wizard configuration flow complete Notification
+	wizflowcomplete: function() {
+		
+		$("<div class='ui-loader ui-overlay-shadow ui-body-e ui-corner-all' id='divtoast'><h3 id='toast'>Configuration Complete</h3></div>")
+		.css({ display: "block",
+			background: "#0C99C9",
+			border: "2px solid black",
+			"border-radius": "10px",
+			"box-shadow": "10px 10px 5px #888888",
+			opacity: 0.90,
+			position: "fixed",
+			padding: "7px",
+			"text-align": "center",
+			"z-index": "19020",		// Higher that the WFO navigation bar of 19000 & 19020
+			color:"white",
+			width: "270px",
+			left: ($(window).width() - 284)/2,
+			top: $(window).height()/2})
+		.appendTo("body")
+		.fadeOut(0)
+		.fadeIn(500, function() {
+		// fadeIn Complete
+		})    
+		.delay(3000)
+		.fadeOut(400, function(){
+			$(this).remove();
+		});
+	},
+	
+	// Show Wizard menu
+    wizmenu: function(x, y, menu) {
+	
+		$("<div class='ui-loader ui-overlay-shadow ui-body-e ui-corner-all' id='divwizmenu'><h3>" + menu + "</h3></div>")
+		.css({ display: "block",
+			background: "#0C99C9",
+			border: "2px solid black",
+			"border-radius": "10px",
+			"box-shadow": "10px 10px 5px #888888",
+			opacity: 0.90,
+			position: "fixed",
+			padding: "7px",
+			"text-align": "center",
+			color:"white",
+			width: "270px",
+			left: x,
+			top: y })
+		.appendTo("body")
+		.fadeOut(0)
+		.fadeIn(300, function() {
+			// fadeIn Complete
+		})    
+	}
+	
 }
-var wizardWin = null;
-if (wizardWin === null) {
-	wizardWin = this;
-}
-wizardeventhook();
+
+// Hook the web wizard into this web application
+WEBWIZ.initialize(this);
 
 /*
- * TODO's
- * 1. Make the wizard menu and toast more beautiful.
- * 2. Choose a configuration work flow and add to work flow JSON file.
- * 
  * Notes.
  * 1. The event interception in this project can also be used to track and report user workflows 
  *    independently of the wizard.
@@ -396,11 +403,14 @@ wizardeventhook();
  * 2. The event interception in this project can also be used to create a user inteface for creating 
  *    the wizard flows, allowing wizard flows to be created or updated without requiring any coding skills.
  * 
- * Additionally required for productization
- * a. Encapsulate wizard code inside JavaScript object name space.
- * b. Allow toast notification locations be to determined relative to target UI item. 
- * c. Implement a more sophisticated proceed algorithm to use a CSS selector expression.
- * d. Update the code to read the JSON work flow configuration from the host web server. 
- * e. Consider creating a UI for the creation of the JSON work flow configuration.
- * f. Add additional configuration work flows.
+ * Next Steps for Productization
+ *   a. Allow toast notification locations be to determined relative to target UI item. 
+ *   b. Implement a more sophisticated proceed algorithm to use a CSS selector expression.
+ *   c. Consider creating a UI for the creation of the JSON work flow configuration. 
+ *      i)  This should include CRUD of wizard workflows and steps.
+ *      ii) Ability to identify an element to proceed to next step in workflow by simply selecting it.
+ *      The two items above will greatly reduce the cost of having to manually code steps and element 
+ *      identification that is assoicated with the high cost of developing RAF automation actions.
+ *  
+ *   d. Update the code to read the JSON work flow configuration from the host web server.
  */
